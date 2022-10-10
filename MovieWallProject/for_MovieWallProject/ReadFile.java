3a5471f9f0394d0a17a7ac25ec914796af10b8ed
blaydeomura <blavde@gmail.com>
2022-10-10 16:24:04 -0700

    2nd commit. Bug with while loop.

--- MovieWallProject/for_MovieWallProject/ReadFile.java
+++ MovieWallProject/for_MovieWallProject/ReadFile.java
@@ -31,29 +31,42 @@ public class ReadFile {
 				String[] values = line.split("\\["); //values[1] gets the 3rd column
 
 				//Need to convert castColumn to String
-				String castColumnString = values[1];
-				
-				//initilize castColumnString to be split into castColumnArr to split again
-				String[] castColumnArr;
-				
-				//Creating arr where each field is seperated into arr
-				//essentially reading JSON file part
-				castColumnArr = castColumnString.split(":|,");
+				String cast = values[1];
 				
 				
-				System.out.println(line); //printing out original first line
-				System.out.println(castColumnString); // printing out cast column of first line (test)
+				int startChar = cast.indexOf("character"); // returns index of character starts
 				
-				System.out.println(castColumnArr[2]); //value we want to search for (test)
-				System.out.println(castColumnArr[3]); //value we want when ""character"" is found
+				ArrayList<String> list = new ArrayList<String>();
 				
-				for (int i = 0; i < castColumnArr.length; i++) { //iterate through castColumnArr
-					if (castColumnArr[i].equals("\"\"character\"\"")) { //if castColumnArr[i] = to ""character""
-						listTest.add(castColumnArr[i+1]); //add value next of it to listTest
-					}
+				int startCharName = startChar + 15; //beggining of character Name 
+				
+				int startName = cast.indexOf("name");
+				
+				int startActorName = startName + 10;
+				
+				
+				while (startChar > 0 ) { //change upper bounds to 0
+					int endChar = cast.indexOf("\"", startCharName); //indexOf(string, start search from)
+					
+					String role = cast.substring(startCharName, endChar); //(start index, end index)
+					list.add(role); //just checking to see if any value recorded
+					
+					
+					int endActorName = cast.indexOf("\"", startActorName);
+					
+					String nameList = cast.substring(startActorName, endActorName);
+					
+					list.add(nameList);
+					
+					break;
+					
 				}
 				
-				System.out.println(listTest.get(0)); //testing to see if anything was added to arrayList
+				System.out.println(line);
+				
+				System.out.println(cast);
+		
+				System.out.println(list.toString());
 
 				
 				break; //i know you hate this, but just to test first line of file
@@ -74,3 +87,27 @@ public class ReadFile {
 		
 
 }}
+
+
+
+//initilize castColumnString to be split into castColumnArr to split again
+//String[] castColumnArr;
+
+////Creating arr where each field is seperated into arr
+////essentially reading JSON file part
+//castColumnArr = castColumnString.split(":|,");
+//
+//
+//System.out.println(line); //printing out original first line
+//System.out.println(castColumnString); // printing out cast column of first line (test)
+//
+//System.out.println(castColumnArr[2]); //value we want to search for (test)
+//System.out.println(castColumnArr[3]); //value we want when ""character"" is found
+//
+//for (int i = 0; i < castColumnArr.length; i++) { //iterate through castColumnArr
+//	if (castColumnArr[i].equals("\"\"character\"\"")) { //if castColumnArr[i] = to ""character""
+//		listTest.add(castColumnArr[i+1]); //add value next of it to listTest
+//	}
+//}
+//
+//System.out.println(listTest.get(0)); //testing to see if anything was added to arrayList
